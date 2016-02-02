d14f744a28ca837f54fa15f413da87bf95aeeb7c
Sam <savaka2@users.noreply.github.com>
2016-02-02 18:19:02 -0500

    Fix auto siteswitchers

--- siteswitcherscript.js
+++ siteswitcherscript.js
@@ -15,28 +15,41 @@ function switchSite() {
 	}
 }
 
-var ss-projects = [
-	'<a href="scratch-extensions/index.html">Scratch Extensions</a>',
-	'<a href="ext-open/index.html">Ext Open</a>'
-];
-
-var ss-more = [
-	'<a href="sbx-files/index.html">SBX Files</a>',
-	'<a href="something/index.html">Something</a>',
-	'<a href="scratch-extensions-directory/index.html">Scratch Extensions Directory</a>',
-	'<a href="blog/index.html">Blog</a>'
-];
-
-var list-projects = document.getElementById('ss-list-projects');
-var list-more = document.getElementById('ss-list-more');
-var item;
-for (i = 0; i < ss-projects.length; i++) {
-	item = document.createElement('li');
-	item.innerHTML = ss-projects[i];
-	list-projects.appendChild(item);
-}
-for (i = 0; i < ss-more.length; i++) {
-	item = document.createElement('li');
-	item.innerHTML = ss-more[i];
-	list-more.appendChild(item);
-}
+window.onload = function() {
+	if (document.getElementById('ss-home') != null) {
+		var ss_projects = [
+			'<a href="scratch-extensions/index.html">Scratch Extensions</a>',
+			'<a href="ext-open/index.html">Ext Open</a>'
+		];
+		var ss_more = [
+			'<a href="sbx-files/index.html">SBX Files</a>',
+			'<a href="something/index.html">Something</a>',
+			'<a href="scratch-extensions-directory/index.html">Scratch Extensions Directory</a>',
+			'<a href="blog/index.html">Blog</a>'
+		];
+	} else {
+		var ss_projects = [
+			'<a href="../scratch-extensions/index.html">Scratch Extensions</a>',
+			'<a href="../ext-open/index.html">Ext Open</a>'
+		];
+		var ss_more = [
+			'<a href="../sbx-files/index.html">SBX Files</a>',
+			'<a href="../something/index.html">Something</a>',
+			'<a href="../scratch-extensions-directory/index.html">Scratch Extensions Directory</a>',
+			'<a href="../blog/index.html">Blog</a>'
+		];
+	}
+	var list_projects = document.getElementById('ssProjects');
+	var list_more = document.getElementById('ssMore');
+	var item;
+	for (i = 0; i < ss_projects.length; i++) {
+		item = document.createElement('li');
+		item.innerHTML = ss_projects[i];
+		list_projects.appendChild(item);
+	}
+	for (i = 0; i < ss_more.length; i++) {
+		item = document.createElement('li');
+		item.innerHTML = ss_more[i];
+		list_more.appendChild(item);
+	}
+};
